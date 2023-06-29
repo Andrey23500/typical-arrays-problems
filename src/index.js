73da1ef553896cf57cafb2bfa61f60b5fec17876
Andrey <andreyavs.00@gmail.com>
2023-06-29 16:42:38 +0300

    feat: refactor code

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 
-exports.min = function min (array) {
-  if (!array || array.length===0) { 
-   return 0;
-  }
-
-  let min=array[0];
-  for(let i=1;i<array.length; i++) {
-    if(min>array[i])min=array[i];
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
     }
-  return min;  
-  
+
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+
 }
 
-exports.max = function max (array) {
-  if (!array || array.length===0) { 
-    return 0;
-  }
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
 
-  let max=array[0];
-  for(let i=1;i<array.length; i++) {
-    if(max<array[i])max=array[i];
-  }
-  return max;
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  if (!array || array.length===0) { 
-    return 0;
-   }
-   
-  let sum=0;
-  let count=0;
-  for(let i=0;i<array.length; i++) {
-    sum+=array[i];
-    count++;
-  }
-  return sum/count;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        count++;
+    }
+    return sum / count;
 }
